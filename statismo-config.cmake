925d931f11986f5bff02c4f0244a2dd82356f713
luethi <marcel.luethi@unibas.ch>
2012-04-02 08:24:32 +0200

    changed statismo-config.cmake to look for hdf5 library in statismo 3rdparty dir (currently linux only)

--- statismo-config.cmake
+++ statismo-config.cmake
@@ -1,18 +1,33 @@
 
 # hdf5 stuff
 
-SET(HDF5_USE_STATIC_LIBRARIES FALSE)
-IF(WIN32)
-  FIND_PACKAGE(HDF5 COMPONENTS CXX REQUIRED NO_MODULE)
-ELSE(WIN32)
-  FIND_PACKAGE(HDF5 COMPONENTS CXX )
-  IF (NOT HDF5_FOUND)
-  FIND_PACKAGE(HDF5 COMPONENTS CXX REQUIRED NO_MODULE)
-  ENDIF(NOT HDF5_FOUND)
-ENDIF(WIN32)
-   
+SET (HDF5_INCLUDE_DIR ${STATISMO_DIR}/3rdParty/hdf5/include)
+SET (HDF5_INCLUDE_DIR_CPP ${HDF5_INCLUDE_DIR}/cpp)
 
+IF(${CMAKE_SYSTEM_NAME} MATCHES "WIN32")
+	if(CMAKE_SIZEOF_VOID_P MATCHES 4)
+		SET (HDF5_LIBRARY_DIR ${STATISMO_DIR}/3rdParty/hdf5/lib/win32)			
+   else()
+		SET (HDF5_LIBRARY_DIR ${STATISMO_DIR}/3rdParty/hdf5/lib/win64)
+   endif()
+	SET (HDF5_LIBRARIES hdf5 hdf5_cpp)
+ELSEIF(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
+	if(CMAKE_SIZEOF_VOID_P MATCHES 4)	
+		SET (HDF5_LIBRARY_DIR ${STATISMO_DIR}/3rdParty/hdf5/lib/linux32)
+	else()
+		SET (HDF5_LIBRARY_DIR ${STATISMO_DIR}/3rdParty/hdf5/lib/linux64)		
+	endif()
+	SET (HDF5_LIBRARIES hdf5 hdf5_cpp)
+ELSE()
+	message(FATAL_ERROR 
+		"The system ${CMAKE_SYSTEM_NAME} is currently not supported by the build system."
+		"The problem is that the hdf5 libraries are not yet included in the statismo distribution."
+		"You can change this yourself, by copy the necessary hdf5 libraries in ${STATISMO_DIR}/3rdParty/hdf5/lib/YOUR_SYSTEM"
+		" and adapt the ${STATISMO_DIR}/statismo-config.cmake file. Please also report to the developers,"
+		"such that your system can be supported in future versions")
+ENDIF() 
 
 SET(STATISMO_LIBRARIES   ${HDF5_LIBRARIES})
 SET(STATISMO_INCLUDE_DIR ${STATISMO_DIR} ${STATISMO_DIR}/3rdParty ${HDF5_INCLUDE_DIR} ${HDF5_INCLUDE_DIR_CPP})
 SET(STATISMO_LIBRARY_DIR  ${HDF5_LIBRARY_DIR})
+	
