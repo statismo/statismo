2b6432e5bdb5ffd7db250b27ad760a033d56dd9d
luethi <marcel.luethi@unibas.ch>
2012-04-11 11:27:50 +0200

    added top level makefile, that performs a superbuild and an installation

--- statismo-config.cmake
+++ statismo-config.cmake
@@ -1,31 +1,10 @@
 
 # hdf5 stuff
 
-SET (HDF5_INCLUDE_DIR ${STATISMO_DIR}/3rdParty/hdf5/include)
+SET (HDF5_INCLUDE_DIR ${STATISMO_DIR}/3rdParty/HDF5/include)
 SET (HDF5_INCLUDE_DIR_CPP ${HDF5_INCLUDE_DIR}/cpp)
-
-IF(${CMAKE_SYSTEM_NAME} MATCHES "WIN32")
-	if(CMAKE_SIZEOF_VOID_P MATCHES 4)
-		SET (HDF5_LIBRARY_DIR ${STATISMO_DIR}/3rdParty/hdf5/lib/win32)			
-   else()
-		SET (HDF5_LIBRARY_DIR ${STATISMO_DIR}/3rdParty/hdf5/lib/win64)
-   endif()
-	SET (HDF5_LIBRARIES hdf5 hdf5_cpp)
-ELSEIF(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
-	if(CMAKE_SIZEOF_VOID_P MATCHES 4)	
-		SET (HDF5_LIBRARY_DIR ${STATISMO_DIR}/3rdParty/hdf5/lib/linux32)
-	else()
-		SET (HDF5_LIBRARY_DIR ${STATISMO_DIR}/3rdParty/hdf5/lib/linux64)		
-	endif()
-	SET (HDF5_LIBRARIES hdf5 hdf5_cpp)
-ELSE()
-	message(FATAL_ERROR 
-		"The system ${CMAKE_SYSTEM_NAME} is currently not supported by the build system."
-		"The problem is that the hdf5 libraries are not yet included in the statismo distribution."
-		"You can change this yourself, by copy the necessary hdf5 libraries in ${STATISMO_DIR}/3rdParty/hdf5/lib/YOUR_SYSTEM"
-		" and adapt the ${STATISMO_DIR}/statismo-config.cmake file. Please also report to the developers,"
-		"such that your system can be supported in future versions")
-ENDIF() 
+SET (HDF5_LIBRARY_DIR ${STATISMO_DIR}/3rdParty/HDF5/lib)
+SET (HDF5_LIBRARIES hdf5 hdf5_cpp)
 
 SET(STATISMO_LIBRARIES   ${HDF5_LIBRARIES})
 SET(STATISMO_INCLUDE_DIR ${STATISMO_DIR} ${STATISMO_DIR}/3rdParty ${HDF5_INCLUDE_DIR} ${HDF5_INCLUDE_DIR_CPP})
