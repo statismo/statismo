de45413a4a0f77ba16ae52b675e76e88178c6632
luethi <marcel.luethi@unibas.ch>
2012-06-15 11:11:48 +0200

    Fixed ITK Python Wrapping (wrapITK) for ITK 4.1

--- Wrapping/ITK/wrap_itkStatisticalModel.cmake
+++
@@ -1,8 +0,0 @@
-wrap_include("itkImageRepresenter.h")
-wrap_include("itkVectorImageRepresenter.h")
-
-WRAP_CLASS("itk::StatisticalModel" POINTER)
-	WRAP_TEMPLATE("IRF2" "itk::ImageRepresenter<float, 2 >")
-	WRAP_TEMPLATE("VIRF22" "itk::VectorImageRepresenter<float, 2, 2 >")
-	WRAP_TEMPLATE("VIRF33" "itk::VectorImageRepresenter<float, 3, 3 >")
-END_WRAP_CLASS()
