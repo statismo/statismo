ce65d4452b57a8a83dcffece0957249dab1d7bd7
Marcel Luethi <marcel.luethi@unibas.ch>
2013-06-07 09:58:23 +0200

    Introduced Posterior Model Builder, which replaces the PartiallyFixedModelBuilder.
    We have refactored the PartiallyFixedModelBuilder such that the code closely corresponds to the theory and notation used in our Media paper "Posterior Shape Models". To make the connection more clear, we have saved it under the new name PosteriorModelBuilder. The PartiallyFixedModelBuilder is deprecated and will in future versions be removed.

--- Wrapping/SWIG/statismo.i
+++ Wrapping/SWIG/statismo.i
@@ -59,6 +59,7 @@
 #include "statismo/DataManagerWithSurrogates.h"
 #include "statismo/StatisticalModel.h"
 #include "statismo/PartiallyFixedModelBuilder.h"
+#include "statismo/PosteriorModelBuilder.h"
 #include "statismo/ReducedVarianceModelBuilder.h"
 #include "statismo/PCAModelBuilder.h"
 #include "statismo/Exceptions.h"
@@ -451,6 +452,43 @@ public:
 %template(PartiallyFixedModelBuilder_vtkUG) statismo::PartiallyFixedModelBuilder<vtkUnstructuredGridRepresenter>;
 %template(PartiallyFixedModelBuilder_vtkSPF3) statismo::PartiallyFixedModelBuilder<vtkStructuredPointsRepresenter<float, 3> >;
 %template(PartiallyFixedModelBuilder_vtkSPSS1) statismo::PartiallyFixedModelBuilder<vtkStructuredPointsRepresenter<signed short, 1> >;
+
+//////////////////////////////////////////////////////
+// PosteriorModelBuilder
+//////////////////////////////////////////////////////
+
+
+namespace statismo { 
+%newobject *::BuildNewModelFromModel;
+%newobject *::BuildNewModel;
+
+template <typename Representer>
+class PosteriorModelBuilder {
+	typedef ModelBuilder<Representer> Superclass;
+public:
+	typedef  DataManager<Representer> 				DataManagerType;
+	typedef typename DataManagerType::SampleDataStructureListType SampleDataStructureListType;		
+	typedef  StatisticalModel<Representer> 	StatisticalModelType;	
+	typedef typename StatisticalModelType::PointValueType PointValueType;
+	typedef  typename StatisticalModelType::PointValueListType PointValueListType;
+	
+	%newobject Create;
+	static PosteriorModelBuilder* Create();
+	virtual ~PosteriorModelBuilder();
+
+	StatisticalModelType* BuildNewModelFromModel(const StatisticalModelType* model,	const PointValueListType& pointValues,  double pointValuesNoiseVariance, bool computeScores=true) const;
+	StatisticalModelType* BuildNewModel(const SampleDataStructureListType& sampleList, const PointValueListType& pointValues,  double pointValuesNoiseVariance,	double noiseVariance) const;
+
+	private:
+		PosteriorModelBuilder();
+};
+}
+
+%template(PosteriorModelBuilder_tvr) statismo::PosteriorModelBuilder<TrivialVectorialRepresenter>;
+%template(PosteriorModelBuilder_vtkPD) statismo::PosteriorModelBuilder<vtkPolyDataRepresenter>;
+%template(PosteriorModelBuilder_vtkUG) statismo::PosteriorModelBuilder<vtkUnstructuredGridRepresenter>;
+%template(PosteriorModelBuilder_vtkSPF3) statismo::PosteriorModelBuilder<vtkStructuredPointsRepresenter<float, 3> >;
+%template(PosteriorModelBuilder_vtkSPSS1) statismo::PosteriorModelBuilder<vtkStructuredPointsRepresenter<signed short, 1> >;
 
 
 //////////////////////////////////////////////////////
---
+++ statismo_ITK/itkPosteriorModelBuilder.h
@@ -0,0 +1,126 @@
+/*
+ * This file is part of the statismo library.
+ *
+ * Author: Marcel Luethi (marcel.luethi@unibas.ch)
+ *
+ * Copyright (c) 2011 University of Basel
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * Neither the name of the project's author nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+
+#ifndef ITK_POSTERIOR_MODELBUILDER_H_
+#define ITK_POSTERIOR_MODELBUILDER_H_
+
+#include "itkObject.h"
+#include "statismoITKConfig.h"
+#include "itkDataManager.h"
+#include "itkStatisticalModel.h"
+#include "statismo/PosteriorModelBuilder.h"
+
+
+namespace itk
+{
+
+/**
+ * \brief ITK Wrapper for the statismo::PosteriorModelBuilder class.
+ * \see statismo::PosteriorModelBuilder for detailed documentation.
+ */
+template <class Representer>
+class PosteriorModelBuilder : public Object {
+public:
+
+	typedef PosteriorModelBuilder            Self;
+	typedef Object	Superclass;
+	typedef SmartPointer<Self>                Pointer;
+	typedef SmartPointer<const Self>          ConstPointer;
+
+	itkNewMacro( Self );
+	itkTypeMacro( PosteriorModelBuilder, Object );
+
+	typedef statismo::PosteriorModelBuilder<Representer> ImplType;
+	typedef statismo::DataManager<Representer> DataManagerType;
+	typedef typename DataManagerType::SampleDataStructureListType SampleDataStructureListType;
+
+
+
+	template <class F>
+	typename std::tr1::result_of<F()>::type callstatismoImpl(F f) const {
+		try {
+			  return f();
+		}
+		 catch (statismo::StatisticalModelException& s) {
+			itkExceptionMacro(<< s.what());
+		}
+	}
+
+
+	PosteriorModelBuilder() : m_impl(ImplType::Create()) {}
+
+	virtual ~PosteriorModelBuilder() {
+		if (m_impl) {
+			delete m_impl;
+			m_impl = 0;
+		}
+	}
+
+
+	// create statismo stuff
+	typedef typename Representer::ValueType ValueType;
+	typedef typename Representer::PointType PointType;
+	typedef typename statismo::PosteriorModelBuilder<Representer>::PointValueListType PointValueListType;
+
+
+	typename StatisticalModel<Representer>::Pointer BuildNewModelFromModel(const StatisticalModel<Representer>* model, const PointValueListType& pointValues, double pointValuesNoiseVariance,  bool computeScores=true) {
+		statismo::StatisticalModel<Representer>* model_statismo = model->GetstatismoImplObj();
+		statismo::StatisticalModel<Representer>* new_model_statismo = callstatismoImpl(std::tr1::bind(&ImplType::BuildNewModelFromModel, this->m_impl, model_statismo, pointValues, pointValuesNoiseVariance, computeScores));
+		typename StatisticalModel<Representer>::Pointer model_itk = StatisticalModel<Representer>::New();
+		model_itk->SetstatismoImplObj(new_model_statismo);
+		return model_itk;
+	}
+
+	typename StatisticalModel<Representer>::Pointer BuildNewModel(SampleDataStructureListType SampleDataStructureList, const PointValueListType& pointValues, double pointValuesNoiseVariance, double noiseVariance) {
+		statismo::StatisticalModel<Representer>* model_statismo = callstatismoImpl(std::tr1::bind(&ImplType::BuildNewModel, this->m_impl, SampleDataStructureList ,pointValues, pointValuesNoiseVariance, noiseVariance));
+		typename StatisticalModel<Representer>::Pointer model_itk = StatisticalModel<Representer>::New();
+		model_itk->SetstatismoImplObj(model_statismo);
+		return model_itk;
+	}
+
+private:
+	PosteriorModelBuilder(const PosteriorModelBuilder& orig);
+	PosteriorModelBuilder& operator=(const PosteriorModelBuilder& rhs);
+
+	ImplType* m_impl;
+};
+
+
+}
+
+#endif /* ITK_POSTERIOR_MODEL_BUILDER */
