8a1498b669bdf54d2bcd912b766aadaad73444d7
Marcel Luethi <marcel.luethi@unibas.ch>
2013-08-30 10:38:06 +0200

    New examples that shows how deformationmodels can be created using the Gaussian Process approach.

---
+++ Examples/ITK/buildGaussianProcessDeformationModel.cpp
@@ -0,0 +1,123 @@
+/*
+ * This file is part of the statismo library.
+ *
+ * Author: Marcel Luethi (marcel.luethi@unibas.ch)
+ *
+ * Copyright (c) 2011 University of Basel
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * Neither the name of the project's author nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+
+#include "itkStandardImageRepresenter.h"
+#include "itkImageRepresenter.h"
+#include "statismo_ITK/itkStatisticalModel.h"
+#include "statismo_ITK/itkLowRankGPModelBuilder.h"
+#include "statismo_ITK/itkDataManager.h"
+#include "itkImageFileReader.h"
+#include "itkDirectory.h"
+#include <sys/types.h>
+#include <errno.h>
+#include <iostream>
+
+/*
+ * This example shows the ITK Wrapping of statismo can be used to build a deformation model.
+ */
+
+
+typedef itk::Image<float, 3> ImageType3D;
+typedef itk::Image< itk::Vector<float, 3> ,3 > VectorImageType3D;
+typedef itk::StandardImageRepresenter<itk::Vector<float, 3>, 3> RepresenterType3D;
+
+typedef itk::Image<float, 2> ImageType2D;
+typedef itk::Image< itk::Vector<float, 2> ,2 > VectorImageType2D;
+typedef itk::StandardImageRepresenter<itk::Vector<float, 2>, 2> RepresenterType2D;
+
+
+
+template <class RepresenterType, class ImageType>
+void itkExample(const char* referenceFilename, double gaussianKernelSigma, const char* modelname) {
+
+
+
+	typedef itk::LowRankGPModelBuilder<ImageType> ModelBuilderType;
+	typedef itk::StatisticalModel<ImageType> StatisticalModelType;
+    typedef std::vector<std::string> StringVectorType;
+	typedef itk::ImageFileReader<ImageType> ImageFileReaderType;
+
+
+    // we take an arbitrary dataset as the reference, as they have all the same resolution anyway
+	typename ImageFileReaderType::Pointer refReader = ImageFileReaderType::New();
+	refReader->SetFileName(referenceFilename);
+	refReader->Update();
+
+    typename RepresenterType::Pointer representer = RepresenterType::New();
+    representer->SetReference(refReader->GetOutput());
+
+
+	const statismo::GaussianKernel gk = statismo::GaussianKernel(gaussianKernelSigma); // a gk with sigma 100
+	// make the kernel matrix valued and scale it by a factor of 100
+	const statismo::MatrixValuedKernel& mvGk = statismo::UncorrelatedMatrixValuedKernel(&gk, representer->GetDimensions());
+	const statismo::MatrixValuedKernel& scaledGk = statismo::ScaledKernel(&mvGk, 100.0);
+
+
+    typename ModelBuilderType::Pointer gpModelBuilder = ModelBuilderType::New();
+    gpModelBuilder->SetRepresenter(representer);
+    typename StatisticalModelType::Pointer model = gpModelBuilder->BuildNewZeroMeanModel(scaledGk, 100);
+    model->Save(modelname);
+
+
+}
+
+int main(int argc, char* argv[]) {
+
+	if (argc < 5) {
+		std::cout << "usage " << argv[0] << " dimension referenceFilename gaussianKernelSigma modelname" << std::endl;
+		exit(-1);
+	}
+
+	unsigned int dimension = atoi(argv[1]);
+	const char* referenceFilename = argv[2];
+	double gaussianKernelSigma = atof(argv[3]);
+	const char* modelname = argv[4];
+
+	if (dimension==2){
+	  itkExample<RepresenterType2D, VectorImageType2D>(referenceFilename, gaussianKernelSigma,  modelname);
+	}
+	else if (dimension==3){
+	  itkExample<RepresenterType3D, VectorImageType3D>(referenceFilename, gaussianKernelSigma, modelname);
+	}
+	else{
+	  assert(0);
+	}
+
+	std::cout << "Model building is completed successfully." << std::endl;
+}
+
