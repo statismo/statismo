fccfbd0a4fdac86174d594577701cb785e3751ea
Marcel Luethi <marcel.luethi@unibas.ch>
2014-09-15 00:31:31 +0200

    boost 1.56

--- superbuild/External-Boost.cmake
+++ superbuild/External-Boost.cmake
@@ -23,8 +23,8 @@ set( Boost_Patch_Command ${SHELL_CMD} ${Boost_Patch_Script} )
 
 ExternalProject_Add(Boost
   BUILD_IN_SOURCE 1
-  URL "http://sourceforge.net/projects/boost/files/boost/1.54.0/boost_1_54_0.tar.gz"
-  URL_MD5 efbfbff5a85a9330951f243d0a46e4b9
+  URL "http://sourceforge.net/projects/boost/files/boost/1.56.0/boost_1_56_0.tar.gz"
+  URL_MD5 8c54705c424513fa2be0042696a3a162
   UPDATE_COMMAND ""
   PATCH_COMMAND ${Boost_Patch_Command}
   CONFIGURE_COMMAND ${Boost_Bootstrap_Command} --prefix=${INSTALL_DEPECENCIES_DIR}/lib --with-libraries=thread --with-libraries=system
