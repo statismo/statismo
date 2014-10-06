715494e31494482485a42cc395c037a93a5ef5a0
Arnaud Gelas <arnaudgelas@gmail.com>
2014-10-06 15:46:35 +0200

    Use boost 1.56
    compilation errors are fixed for XCode >= 5.1

--- superbuild/External-Boost.cmake
+++ superbuild/External-Boost.cmake
@@ -10,24 +10,18 @@ set( Boost_Bootstrap_Command )
 # while on Windows, we need CR/LF line feeds (only available in the .zip)
 
 if( UNIX )
-  set( Boost_url "http://sourceforge.net/projects/boost/files/boost/1.55.0/boost_1_55_0.tar.gz")
-  set( Boost_md5 93780777cfbf999a600f62883bd54b17)
+  set( Boost_url "http://sourceforge.net/projects/boost/files/boost/1.56.0/boost_1_56_0.tar.gz")
+  set( Boost_md5 8c54705c424513fa2be0042696a3a162 )
   set( Boost_Bootstrap_Command ./bootstrap.sh )
   set( Boost_b2_Command ./b2 )
-  set( SHELL_CMD sh)
 else()
   if( WIN32 )
-    set( Boost_url "http://sourceforge.net/projects/boost/files/boost/1.55.0/boost_1_55_0.zip")
-    set( Boost_md5 8aca361a4713a1f491b0a5e33fee0f1f)
+    set( Boost_url "http://sourceforge.net/projects/boost/files/boost/1.56.0/boost_1_56_0.zip")
+    set( Boost_md5 fc16da21d641e03715a4144cb093964e )
     set( Boost_Bootstrap_Command cmd /C bootstrap.bat msvc )
     set( Boost_b2_Command b2.exe )
-    set (SHELL_CMD "")
   endif()
 endif()
-
-set( Boost_Patches_DIR ${Patches_DIR}/boost )
-set( Boost_Patch_Script ${Boost_Patches_DIR}/boost_patch.sh )
-set( Boost_Patch_Command ${SHELL_CMD} ${Boost_Patch_Script} )
 
 if(CMAKE_SIZEOF_VOID_P EQUAL 8)
   set(Boost_address_model 64)
@@ -40,7 +34,6 @@ ExternalProject_Add(Boost
   URL ${Boost_url} 
   URL_MD5 ${Boost_md5} 
   UPDATE_COMMAND ""
-  #PATCH_COMMAND ${Boost_Patch_Command}
   CONFIGURE_COMMAND ${Boost_Bootstrap_Command} --prefix=${INSTALL_DEPECENCIES_DIR}/lib
   BUILD_COMMAND ${Boost_b2_Command} install -j8   --prefix=${INSTALL_DEPECENCIES_DIR} --with-thread --with-system address-model=${Boost_address_model}
   INSTALL_COMMAND ""
