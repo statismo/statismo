f92e240a97d239ede441ecd7411a210f86537159
Stefan Schlager <zarquon42@gmail.com>
2015-01-12 15:39:29 +0100

    added option -v in switch-release to specify major vtk version to build against

--- debian/switch-release.sh
+++ debian/switch-release.sh
@@ -1,17 +1,42 @@
 #! /bin/bash
-if [ -z $1 ]; then
-    echo "Usage: switch-release.sh distribution-name"
-    exit
+
+helpcall() {
+    echo "     Usage: switch-release.sh -d distribution-name [-v vtk-major-version]"
+    exit 1
+}
+
+ARGC=$#
+if [ "$#" -lt 2 ]; then
+    helpcall
 fi
-    
-distrib=$1
+
+vtk=5
+
+while getopts "v:d:" opt; do
+    case "$opt" in
+	v)
+            vtk=$OPTARG
+            ;;
+	d)  distrib=$OPTARG
+            ;;
+    esac
+done    
+echo $distrib
+if [ -z $distrib ];then 
+     helpcall
+fi
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $DIR
 echo $DIR
 cp -f changelog changelog.bck
+cp -f control control.bck
+vtk=libvtk$vtk-dev
+echo $vtk
+sed -i "s|libvtk5-dev|$vtk|g" control
 cd ../
 dch -r -D $distrib
 debuild -S
 cd debian
 mv changelog.bck changelog
+mv control.bck control
 
