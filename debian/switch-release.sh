8529d35fd6161a6d14ee6bc18e03a20d5e64db9a
Stefan Schlager <zarquon42@gmail.com>
2015-05-06 10:15:01 +0200

    removed the  Qt5 fixes from debian/control

--- debian/switch-release.sh
+++ debian/switch-release.sh
@@ -30,10 +30,10 @@ cd $DIR
 echo $DIR
 cp -f changelog changelog.bck
 cp -f control control.bck
-cp -f ../CMakeLists.txt ../CMakeLists.txt.bck
-if [ $vtk == 6 ]; then
-sed -i 's|find_package( VTK REQUIRED )|find_package( VTK REQUIRED )\n  find_package(Qt5Widgets REQUIRED)|g' ../CMakeLists.txt
-fi
+#cp -f ../CMakeLists.txt ../CMakeLists.txt.bck
+#if [ $vtk == 6 ]; then
+#sed -i 's|find_package( VTK REQUIRED )|find_package( VTK REQUIRED )\n  find_package(Qt5Widgets REQUIRED) \n  find_package(Qt5OpenGL REQUIRED)|g' ../CMakeLists.txt
+#fi
 vtk=libvtk$vtk-dev
 echo $vtk
 sed -i "s|libvtk5-dev|$vtk|g" control
@@ -43,5 +43,5 @@ debuild -S -I
 cd debian
 mv changelog.bck changelog
 mv control.bck control
-mv ../CMakeLists.txt.bck ../CMakeLists.txt
+#mv ../CMakeLists.txt.bck ../CMakeLists.txt
 
